207008b5c478c51236ecfcb55ac69cded6bcd4a6
victor <victorreisprog@gmail.com>
2025-08-05 08:49:26 -0300

    feat: add vertex

--- graph/main.go
+++ graph/main.go
@@ -4,12 +4,16 @@ import "fmt"
 
 func main() {
 	graph := NewGraph()
-	graph.AddVertex(1).AddVertex(20)
+	graph.AddVertex(1).AddVertex(20).AddVertex(14)
+	graph.PrintGraph()
+	graph.AddEdge(1, 14, 5)
 	graph.PrintGraph()
 }
 
 type IGraph interface {
 	AddVertex(d int) *Graph
+	AddEdge(from, to, weight int) *Edge
+	GetVertex(d int) *Vertex
 	PrintGraph()
 }
 
@@ -18,14 +22,14 @@ type Graph struct {
 	Vertices map[int]*Vertex
 }
 
-type Edges struct {
+type Edge struct {
 	weight int
 	dest   *Vertex
 }
 
 type Vertex struct {
 	val   int
-	edges map[int]*Edges
+	edges map[int]*Edge
 }
 
 func NewGraph() IGraph {
@@ -35,7 +39,7 @@ func NewGraph() IGraph {
 }
 
 func (g *Graph) AddVertex(d int) *Graph {
-	// 1 - se já existir, não cria, retorna o grafico para ficar idempotente
+	// 1 - se já existir, não cria, retorna o grafo para ficar idempotente
 	if _, exists := g.Vertices[d]; exists {
 		return g
 	}
@@ -43,7 +47,7 @@ func (g *Graph) AddVertex(d int) *Graph {
 	// 2 - vertex novo, sem nenhuma conexao ainda...
 	vertex := &Vertex{
 		val:   d,
-		edges: make(map[int]*Edges),
+		edges: make(map[int]*Edge),
 	}
 
 	// 3 - registrar no grafo, adiciona o novo vertice nos vertices
@@ -52,12 +56,71 @@ func (g *Graph) AddVertex(d int) *Graph {
 	return g
 }
 
+func (g *Graph) AddEdge(from, to, weight int) *Edge {
+	// 1 - verificar se ambos os vertices existem
+	if g.Vertices[from] == nil || g.Vertices[to] == nil {
+		fmt.Printf("From or to doesnt exist")
+		return nil
+	}
+
+	edge := Edge{
+		weight: weight,
+		dest:   g.GetVertex(to),
+	}
+
+	count := 0
+	// 2 - verificar se a aresta ja existe
+	for _, vertex := range g.Vertices {
+		count++
+		// se ja existir, atualiza o peso
+		if ed, exists := vertex.edges[to]; exists {
+			fmt.Println("existe, adicionando")
+			*ed = edge
+
+			break
+		}
+
+		// se nao existe cria
+		if count == len(g.Vertices)-1 && vertex.edges[to] == nil {
+			fmt.Println("num existe, criando")
+			vertex.edges = map[int]*Edge{
+				to: &edge,
+			}
+		}
+	}
+
+	// 3 - Armazena o valor no map de arestas do vertice
+	if fr, exists := g.Vertices[from]; exists {
+		fmt.Println("hello world")
+		*fr = Vertex{
+			val: from,
+			edges: map[int]*Edge{
+				from: &edge,
+			},
+		}
+	}
+
+	return &edge
+}
+
+func (g *Graph) GetVertex(d int) *Vertex {
+	if vertex, exists := g.Vertices[d]; exists {
+		return vertex
+	}
+
+	return nil
+}
+
 func (g *Graph) PrintGraph() {
 	if g == nil {
 		return
 	}
 
 	for k, v := range g.Vertices {
-		fmt.Printf("key: %d Vertex: %+v\n", k, v)
+		if edge, exists := v.edges[k]; exists {
+			fmt.Printf("key: %d Vertex Val: %d, Vertex Edge Dest: %d Vertex Edge Weight: %+v\n", k, v.val, edge.dest.val, edge.weight)
+		} else {
+			fmt.Printf("key: %d Vertex Val: %d, Edge address: %v\n", k, v.val, edge)
+		}
 	}
 }
